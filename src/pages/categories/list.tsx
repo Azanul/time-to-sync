669af7109226745874089b59df6da01a05ae1e8b
Azanul <azanulhaque@gmail.com>
2023-07-07 21:03:29 +0530

    category list
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/pages/categories/list.tsx
+++ src/pages/categories/list.tsx
@@ -1,6 +1,68 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { MuiListInferencer } from "@refinedev/inferencer/mui";
+import React from "react";
+import {
+    useDataGrid,
+    EditButton,
+    ShowButton,
+    DeleteButton,
+    List,
+} from "@refinedev/mui";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
 
 export const CategoryList: React.FC<IResourceComponentsProps> = () => {
-  return <MuiListInferencer />;
+    const translate = useTranslate();
+    const { dataGridProps } = useDataGrid();
+
+    const columns = React.useMemo<GridColDef[]>(
+        () => [
+            {
+                field: "name",
+                flex: 1,
+                headerName: translate("categories.fields.name"),
+                minWidth: 200,
+                headerAlign: "center",
+                align: "center"
+            },
+            {
+                field: "color",
+                flex: 1,
+                headerName: translate("categories.fields.color"),
+                type: "text",
+                minWidth: 200,
+                renderCell: function render({ row }) {
+                  return (
+                      <div style={{backgroundColor: "#"+row.color, textAlign: "center", height: "80%", width: "80%"}}>
+                        #{row.color}
+                      </div>
+                  );
+                },
+                headerAlign: "center",
+                align: "center"
+            },
+            {
+                field: "actions",
+                headerName: translate("table.actions"),
+                sortable: false,
+                renderCell: function render({ row }) {
+                    return (
+                        <>
+                            <EditButton hideText recordItemId={row.id} />
+                            <ShowButton hideText recordItemId={row.id} />
+                            <DeleteButton hideText recordItemId={row.id} />
+                        </>
+                    );
+                },
+                align: "center",
+                headerAlign: "center",
+                minWidth: 80,
+            },
+        ],
+        [translate],
+    );
+
+    return (
+        <List>
+            <DataGrid {...dataGridProps} columns={columns} autoHeight  />
+        </List>
+    );
 };
