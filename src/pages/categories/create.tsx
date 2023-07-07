18be89667d7dc6fa411f88329206325246c389a6
Azanul <azanulhaque@gmail.com>
2023-07-07 23:44:49 +0530

    category create
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/pages/categories/create.tsx
+++ src/pages/categories/create.tsx
@@ -1,6 +1,60 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { MuiCreateInferencer } from "@refinedev/inferencer/mui";
+import { Create } from "@refinedev/mui";
+import { Box, TextField } from "@mui/material";
+import { useForm } from "@refinedev/react-hook-form";
+import { IResourceComponentsProps, useTranslate } from "@refinedev/core";
+import { HexColorPicker } from "react-colorful";
 
 export const CategoryCreate: React.FC<IResourceComponentsProps> = () => {
-  return <MuiCreateInferencer />;
+  const translate = useTranslate();
+  const {
+    saveButtonProps,
+    refineCore: { formLoading },
+    register,
+    formState: { errors },
+    setValue,
+    watch
+  } = useForm();
+
+  return (
+    <Create isLoading={formLoading} saveButtonProps={saveButtonProps}>
+      <Box
+        component="form"
+        sx={{ display: "flex", flexDirection: "column" }}
+        autoComplete="off"
+      >
+        <TextField
+          {...register("name", {
+            required: "This field is required",
+          })}
+          error={!!(errors as any)?.name}
+          helperText={(errors as any)?.name?.message}
+          margin="normal"
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+          type="text"
+          label={translate("categories.fields.name")}
+          name="name"
+        />
+        <TextField
+          {...register("color", {
+            required: "This field is required",
+          })}
+          error={!!(errors as any)?.color}
+          helperText={(errors as any)?.color?.message}
+          margin="normal"
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+          type="text"
+          label={translate("categories.fields.color")}
+          name="color"
+        />
+        <HexColorPicker
+          color={watch("color")}
+          onChange={(val: any) => {
+            setValue("color", val);
+          }}
+        />
+      </Box>
+    </Create>
+  );
 };
