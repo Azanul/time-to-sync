e2ae6a801827183857930b29b4c3b3cce9417f58
Azanul <azanulhaque@gmail.com>
2023-07-09 12:16:37 +0530

    category show
    Signed-off-by: Azanul <azanulhaque@gmail.com>

--- src/pages/categories/show.tsx
+++ src/pages/categories/show.tsx
@@ -1,6 +1,45 @@
-import { IResourceComponentsProps } from "@refinedev/core";
-import { MuiShowInferencer } from "@refinedev/inferencer/mui";
+import {
+  useShow,
+  IResourceComponentsProps,
+  useTranslate,
+} from "@refinedev/core";
+import {
+  Show,
+  NumberField,
+  DateField,
+  TextFieldComponent as TextField,
+} from "@refinedev/mui";
+import { Typography, Stack } from "@mui/material";
 
 export const CategoryShow: React.FC<IResourceComponentsProps> = () => {
-  return <MuiShowInferencer />;
+  const translate = useTranslate();
+  const { queryResult } = useShow();
+  const { data, isLoading } = queryResult;
+
+  const record = data?.data;
+
+  return (
+    <Show isLoading={isLoading}>
+      <Stack gap={1}>
+        <Typography variant="body1" fontWeight="bold">
+          {translate("categories.fields.id")}
+        </Typography>
+        <NumberField value={record?.id ?? ""} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("categories.fields.created_at")}
+        </Typography>
+        <DateField value={record?.created_at} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("categories.fields.name")}
+        </Typography>
+        <TextField value={record?.name} />
+        <Typography variant="body1" fontWeight="bold">
+          {translate("categories.fields.color")}
+        </Typography>
+        <div style={{ backgroundColor: record?.color, textAlign: "center", height: "80%", width: "80%" }}>
+          {record?.color}
+        </div>
+      </Stack>
+    </Show>
+  );
 };
